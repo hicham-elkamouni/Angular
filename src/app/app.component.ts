6a8477db81b64746bd5dba946cb9a88e832353b7
hicham-elkamouni <hichamelkamouni.dev@gmail.com>
2022-05-08 15:06:54 +0100

    passing data from child to parent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title : string = 'angular-crash';
+  title(title: any) {
+    throw new Error('Method not implemented.');
+  }
 }
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  title : string = 'Task Tracker';
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+  }
+
+  toggleAddTask(){
+    console.log('toggle');
   }
 
 }
