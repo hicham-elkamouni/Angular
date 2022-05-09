98bb7361bd19d1094076458e0222b09effd8a65f
hicham-elkamouni <hichamelkamouni.dev@gmail.com>
2022-05-09 01:40:42 +0100

    adding font awesome and task service

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Task } from '../../Task';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-task-item',
@@ -8,7 +10,7 @@ import { Task } from '../../Task';
 })
 export class TaskItemComponent implements OnInit {
   @Input() task: Task;
-
+  faTimes = faTimes;
 
   constructor() { }
 
